e74de29fdac0cd67c70cd9f666be6c17c1bd4277
jml-moe <2318069@student.itn.ac.id>
2026-03-21 23:04:11 +0700

    remove: delete main.js file and update note.txt with additional JavaScript style recommendations

---
+++ src/traversal.js
@@ -0,0 +1,12 @@
+// perubahan/manipulasi data, eg: penggabungan, slicing
+
+const username = "jamil";
+const myFullName = `${username} ${"Nashrulloh"}`;
+
+console.log(myFullName)
+console.log(myFullName.slice(2, 10))
+
+const capitalize = myFullName.charAt(0).toUpperCase();
+const capitalFullName = `${capitalize}${myFullName.slice(1)}` 
+// .slice(start?: number, end?: number): string, kalau ada question mark-nya maka boleh dikosongkan (opsional)
+console.log(capitalFullName)
