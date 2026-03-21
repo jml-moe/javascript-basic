c4bd26690613269f3fe40eb83a3ad2e6362824e4
jml-moe <2318069@student.itn.ac.id>
2026-03-21 22:34:38 +0700

    refactor: streamline project structure by consolidating JavaScript files

---
+++ main.js
@@ -0,0 +1,25 @@
+const myData = {
+    firtsName: "Jamil",
+    lastName: "Nashrulloh",
+    isStudent: true,
+    isRich: null,
+};
+
+myData.isRich = true; // aamiin
+console.log(myData);
+
+myProgrammingLanguage = ["Javascript", "Python", "Go"];
+
+const myRecipe = [
+    {
+        name: "Nasi Goreng",
+        ingredients: ["bawang putih", "bawang merah", "etc"]
+    },
+    {
+        name: "tahu telor",
+        ingredients: ["Tahu", "Telor"]
+    }
+]
+
+console.log(myProgrammingLanguage[0])
+console.log(myRecipe)
