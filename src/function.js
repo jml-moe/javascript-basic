9c879d5bbe6a887150f953c5734107d528b68507
jml-moe <2318069@student.itn.ac.id>
2026-03-22 00:22:00 +0700

    learn: function

--- src/function.js
+++ src/function.js
@@ -1,17 +1,48 @@
 // reusable block of a unit code
 // named function & arrow function
 
+// named-function
 function add(x, y) {
-    console.log(x + y);
+  console.log(x + y);
 }
 add(1, 2); // 3
 add(10, 2); // 12
 
 function sub(x, y) {
-    return x - y;
+  return x - y;
 }
 
-const resultOne = sub(5, 3) // 2
-const resultTwo = sub(10, 2) // 8
+const resultOne = sub(5, 3); // 2
+const resultTwo = sub(10, 2); // 8
 console.log("Hasil : ", resultOne); // 2
 console.log("Hasil : ", resultOne + resultTwo); // 10, value resultOne bisa dipanggil karena value nya ditampung karena menggunakan return
+
+// arrow-function
+const adds = (x, y) => x + y;
+const subs = (x, y) => x - y;
+
+const resulThree = adds(5, 3);
+const resultFour = subs(5, 3);
+
+console.log("Hasil : ", resulThree); // 8
+console.log("Hasil : ", resultFour); // 2
+
+// EXERCISE
+
+// function capital(username) {
+//   return `${username.charAt(0).toUpperCase()}${username.slice(1)}`;
+// }
+
+// const result_one = capital("syarif");
+// const result_two = capital("fuah");
+// const result_three = capital("jamil");
+// console.log(result_one);
+// console.log(result_two);
+// console.log(result_three);
+
+function capital(username) {
+  console.log(`${username.charAt(0).toUpperCase()}${username.slice(1)}`);
+}
+
+capital("jamil");
+capital("moe");
--- src/traversal.js
+++ src/traversal.js
@@ -3,10 +3,10 @@
 const username = "jamil";
 const myFullName = `${username} ${"Nashrulloh"}`;
 
-console.log(myFullName)
-console.log(myFullName.slice(2, 10))
+console.log(myFullName);
+console.log(myFullName.slice(2, 10));
 
 const capitalize = myFullName.charAt(0).toUpperCase();
-const capitalFullName = `${capitalize}${myFullName.slice(1)}` 
+const capitalFullName = `${capitalize}${myFullName.slice(1)}`;
 // .slice(start?: number, end?: number): string, kalau ada question mark-nya maka boleh dikosongkan (opsional)
-console.log(capitalFullName)
+console.log(capitalFullName);
