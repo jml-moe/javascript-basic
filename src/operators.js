4d939078457e0dacca384b20f129ad3c10f598b8
jml-moe <2318069@student.itn.ac.id>
2026-03-22 18:32:21 +0700

    learn: operators

---
+++ src/operators.js
@@ -0,0 +1,21 @@
+// Comparison
+const myNumber = "3";
+const hisNumber = 3;
+const isTheSame = myNumber == hisNumber;
+const isTheDifference = myNumber !== hisNumber;
+
+console.log(isTheSame);
+console.log(isTheDifference);
+
+// Logic Comparison
+
+// OR ||
+const username = "admin";
+const password = "admin123";
+
+// const isAccountValid = username === "admin" || username === "jamil";
+// console.log(isAccountValid);
+
+// AND &&
+const isAccountValid = username === "admin" || password === "admin123";
+console.log(isAccountValid);
