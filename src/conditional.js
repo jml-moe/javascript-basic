7cada4fa5d733b2f09d1d8241f8074a8390ca4c8
jml-moe <2318069@student.itn.ac.id>
2026-04-02 21:53:38 +0700

    learn: just review

---
+++ src/conditional.js
@@ -0,0 +1,9 @@
+const pointMember = 10000;
+
+if (pointMember >= 10000) {
+  console.log("Advocate");
+} else if (pointMember >= 6000) {
+  console.log("Elite");
+} else {
+  console.log("New");
+}
--- src/operators.js
+++ src/operators.js
@@ -16,6 +16,15 @@ const password = "admin123";
 // const isAccountValid = username === "admin" || username === "jamil";
 // console.log(isAccountValid);
 
+// const loginEmail = "jamil@admin.com";
+const loginEmail = "me@admin.com";
+
+const emailAdmin1 = "jamil@admin.com";
+const emailAdmin2 = "moe@admin.com";
+
+const isValid = loginEmail === emailAdmin1 || loginEmail === emailAdmin2;
+console.log(isValid);
+
 // AND &&
-const isAccountValid = username === "admin" || password === "admin123";
+const isAccountValid = username === "admin" && password === "admin123";
 console.log(isAccountValid);
